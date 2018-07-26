e3afe1320b79e68e489d85082c9a0841e97872a6
Your Name <you@example.com>
2018-07-26 14:11:49 +0800

    'url基本配置'

--- src/DataServer/UrlConfig.js
+++ src/DataServer/UrlConfig.js
@@ -3,24 +3,37 @@ const pore="52263";
 
 const imgUrl=http+pore+'/AvatarImg/';
 const registerUrl=http+pore+'/api/User/Regist';
+const UpdateCustomerUrl=http+pore+'/api/Customer/UpdateCustomer';
+const AddShippingAddressUrl=http+pore+'/api/ShippingAddress/AddShippingAddress';
 const loginUrl=http+pore+'/api/User/Login';
-
-
-
+const DeleteShippingAddressUrl=http+pore+'/api/ShippingAddress/DeleteShippingAddress';
+const UpdateShippingUrl=http+pore+'/api/ShippingAddress/Update';
+const SearchShippingAddressUrl=http+pore+'/api/ShippingAddress/SearchShippingAddress';
+const GetOneShippingUrl=http+pore+'/api/ShippingAddress/GetOne';
+const GetCommodityTypeUrl=http+pore+'/api/CommodityType/GetCommodityType';
+const ProductByCommodityTypeUrl=http+pore+'/api/CommodityType/ProductByCommodityType';
+const ProductByCommodityTypeNameUrl=http+pore+'/api/CommodityType/ProductByCommodityTypeName';
+const SeachProductUrl=http+pore+'/api/CommodityType/SeachProduct';
+const AddOrderUrl=http+pore+'/api/PlaceOrder/AddOrder';
+const SearchOrderUrl=http+pore+'/api/PlaceOrder/SearchOrder';
+const GoodsIsOKUrl=http+pore+'/api/PlaceOrder/GoodsIsOK';
+const IsPaymentUrl=http+pore+'/api/PlaceOrder/IsPayment';
 export {
     registerUrl,
     imgUrl,
     loginUrl,
-    changePasswordUrl,
-    createUserUrl,
-    getUserUrl,
-    updateUserUrl,
-    postMessageUrl,
-    deleteMessageUrl,
-    findUserUrl,
-    followUrl,
-    getFollowUrl,
-    getMessageUrl,
-    homeMessageUrl,
-    unFollowURL
+    UpdateCustomerUrl,
+    AddShippingAddressUrl,
+    DeleteShippingAddressUrl,
+    UpdateShippingUrl,
+    SearchShippingAddressUrl,
+    GetOneShippingUrl,
+    GetCommodityTypeUrl,
+    ProductByCommodityTypeUrl,
+    ProductByCommodityTypeNameUrl,
+    SeachProductUrl,
+    AddOrderUrl,
+    SearchOrderUrl,
+    GoodsIsOKUrl,
+    IsPaymentUrl,
 }
