d77319691ef89979c3c1dfc057e5b23138817e07
Your Name <you@example.com>
2018-08-01 09:13:15 +0800

    '收货地址，个人信息'

---
+++ src/Screen/GetUserScreen.js
@@ -0,0 +1,141 @@
+import React, { Component } from 'react'
+
+import { 
+    Button,
+    Toast,
+    NavBar,
+    WingBlank, 
+    WhiteSpace ,
+    List,
+    InputItem,
+    Flex,
+    ImagePicker,
+    Icon
+} from 'antd-mobile';
+
+
+import{imgUrl} from '../DataServer/UrlConfig';
+import userManager from '../DataServer/UserManager';
+
+export default class MyInfoScreen extends Component {
+
+    async componentDidMount(){
+        // console.log(userManager.ifToken());
+        // if (!UserData.ifToken()) {
+        //     this.props.history.replace('/');
+        // }
+    const result=await userManager.getUser();
+    console.log(result);
+    if(result.success===false){
+        Toast.fail(result.errorMessage,1);
+        return;
+    }
+    this.setState({Nickname:result.data.Nickname,Avatar:result.data.Avatar})
+     console.log(imgUrl+this.state.Avatar)
+}
+//   async componentDidMount(){
+
+//     const result = await userManager.getUser();
+
+//     if(result.success === false){
+//         Toast.fail(result.errorMessage,1);
+//         return;
+//     }
+
+//     this.setState({customer:result.data});
+//   }
+
+
+
+
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+        Nickname:'',
+        Avatar:'',
+    }
+  }
+    
+
+  render() {
+
+    return (
+      <div>
+        <NavBar
+            mode="dark"
+            icon={<Icon type="left" />}
+            onLeftClick={() => { this.props.history.goBack() }}
+                >个人信息</NavBar>
+        {/* <Flex
+          justify={'center'}
+          style={{backgroundColor:'#ffffff'}}
+        >
+          <img
+            alt={''}
+            src={imgUrl+this.state.customer.AvatarImg}
+            style={{width:'100px',height:'100px',margin:'5px'}}
+          /> 
+        </Flex> */}
+        
+        <List>
+        <ImagePicker
+                  
+                  files={[{url:imgUrl+this.state.Avatar}]}
+                  onChange={(Avatar)=>{this.setState({Avatar})}}
+                  selectable={this.state.Avatar.length <= 1}
+                  />
+            <InputItem
+                type={'text'}
+                editable={false}
+                value={this.state.Nickname}
+                onChange={(Nickname)=>{this.setState({Nickname})}}
+                placeholder={'请输入昵称'}
+            >
+                昵称
+            </InputItem>
+        </List>
+        <WhiteSpace/>
+        <WingBlank>
+            <Button
+            type='primary'
+                onClick={async()=>{
+                    this.props.history.push('/UpdateCustomerScreen',this.state.Nickname,this.state.Avatar);
+                }}
+            >
+              修改个人资料
+            </Button>
+            <WhiteSpace/>
+            <Button
+            type='primary'
+                onClick={async()=>{
+                    this.props.history.push('/AddShoppingAddress');
+                }}
+            >
+                设置收货地址
+            </Button>
+            <WhiteSpace/>
+            <Button
+            type='primary'
+                onClick={async()=>{
+                    this.props.history.push('/UpdateShoppingAddressScreen');
+                }}
+            >
+                修改收货地址
+            </Button>
+            <WhiteSpace/>
+            <Button
+                type={'warning'}
+                onClick={async()=>{
+                      userManager.tokenOut();
+                      this.props.history.replace('/');
+                      
+                }}
+            >
+                退出登录
+            </Button>
+        </WingBlank>
+      </div>
+    )
+  }
+}
---
+++ src/Screen/UpdateCustomerScreen.js
@@ -0,0 +1,103 @@
+import React, { Component } from 'react'
+
+import { 
+    Button,
+    Toast,
+    NavBar,
+    WingBlank, 
+    WhiteSpace ,
+    List,
+    InputItem,
+    Icon,
+    ImagePicker,
+    Modal
+} from 'antd-mobile';
+
+import{imgUrl} from '../DataServer/UrlConfig';
+import userManager from '../DataServer/UserManager';
+
+export default class UpdateCustomerScreen extends Component {
+    async componentDidMount(){
+    const result=await userManager.getUser();
+    console.log(result);
+    if(result.success===false){
+        Toast.fail(result.errorMessage,1);
+        return;
+    }
+    this.setState({Nickname:result.data.Nickname})
+    console.log(this.state.files);
+    
+}
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         Nickname:'',
+         files:[]
+      }
+    }
+    
+
+
+  render() {
+    return (
+      <div>
+        <NavBar
+            mode="dark"
+            icon={<Icon type="left" />}
+            onLeftClick={() => {this.props.history.goBack()}}
+        >修改用户信息</NavBar>
+        <WhiteSpace/>
+        <ImagePicker
+            files={this.state.files}
+            onChange={(files)=>{this.setState({files})}}
+            selectable={this.state.files.length<=1}
+            
+        />
+        
+        <List>
+            <InputItem
+                type={'text'}
+                value={this.state.Nickname}
+                onChange={(Nickname)=>{this.setState({Nickname})}}
+                placeholder={'请输入昵称'}
+            >
+                昵称
+            </InputItem>
+        </List>
+        <WhiteSpace/>
+        <WingBlank>
+            <Button
+                type={'primary'}
+                onClick={async()=>{
+
+                    Toast.loading('内容上传中...',0);
+                    const userinfo = {
+                        Nickname:this.state.Nickname
+                    }
+                    if(this.state.files.length !== 0){
+                        userinfo.image = this.state.files;
+                        console.log( userinfo.image)
+                    } 
+                    const result = await userManager.updateUser(userinfo);
+                    console.log(result)
+
+                    Toast.hide();
+                    if(result.success === false){
+                        Toast.fail(result.errorMessage);
+                        return;
+                    }
+                    Modal.alert('修改成功','点击确认返回',[{
+                        text:'确认',
+                        onPress:()=>{this.props.history.goBack()}
+                    }])
+                    
+                }}
+            >
+                提交修改
+            </Button>
+        </WingBlank>
+      </div>
+    )
+  }
+}
